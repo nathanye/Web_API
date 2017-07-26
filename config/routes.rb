34938a5c391a555c6db9c8983abe6e84abb9e3c2
nathanye <khalil54781@gmail.com>
2017-07-26 16:53:46 +0800

    check my ticket

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => { :format => :json } do
     namespace :v1 do
+      get "/reservations" => "reservations#index", :as => :reservations
       get "/trains" => "trains#index", :as => :trains
       get "/trains/:train_number" => "trains#show", :as => :train
 
