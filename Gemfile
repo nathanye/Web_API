a5ee560c069c889ce737e335b91cfc3cea3a5ee9
nathanye <khalil54781@gmail.com>
2017-07-26 20:12:19 +0800

    edit personal message

--- Gemfile
+++ Gemfile
@@ -36,6 +36,10 @@ gem 'rest-client'
 
 gem 'devise'
 
+gem 'carrierwave'
+
+gem 'mini_magick'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
   def generate_authentication_token
     self.authentication_token = Devise.friendly_token
   end
+
+  mount_uploader :avatar, AvatarUploader
 end
