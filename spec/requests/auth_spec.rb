9361b12db5355b225c86ab19337c5aeaa92249aa
nathanye <khalil54781@gmail.com>
2017-07-31 22:08:13 +0800

    testing login/logout API

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -26,4 +26,48 @@ RSpec.describe "API_V1::Auth", :type => :request do
 
     expect(response.body).to eq( { :message => "Failed", :errors => {:password => ["can't be blank"]}  }.to_json )
   end
+
+    before do
+    @user = User.create!( :email => "test@example.com", :password => "12345678")
+  end
+
+  example "valid login and logout" do
+    post "/api/v1/login", params: { :auth_token => @user.authentication_token,
+                                    :email => @user.email, :password => "12345678" }
+
+    expect(response).to have_http_status(200)
+    # 检查回传的 JSON
+
+    expect(response.body).to eq(
+      {
+        :message => "Ok",
+        :auth_token => @user.authentication_token,
+        :user_id => @user.id
+      }.to_json
+    )
+
+    post "/api/v1/logout"
+    expect(response).to have_http_status(401)
+
+    post "/api/v1/logout", params: { :auth_token => @user.authentication_token }
+    expect(response).to have_http_status(200)
+    old_token = @user.authentication_token
+    @user.reload
+    # 检查登出后 `authentication_token` 会改掉
+
+    expect(@user.authentication_token).not_to eq(old_token)
+  end
+
+  example "invalid auth token login" do
+    post "/api/v1/login", params: { :auth_token => @user.authentication_token,
+                                    :email => @user.email, :password => "xxx" }
+
+    # 检查登入失败回传 401
+
+    expect(response).to have_http_status(401)
+    expect(response.body).to eq(
+      { :message => "Email or Password is wrong" }.to_json
+    )
+  end
+
 end
